7f4f28584cfd1059d1b60657970368464f811c93
Panna Somlo <psomlo97@gmail.com>
2020-10-26 22:17:14 +0100

    Add NewsPortal.Dal

---
+++ NewsPortal.Dal/Entities/News.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPortal.Dal.Entities
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Headline { get; set; }
+        public string ShortDescription { get; set; }
+        public string Body { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ NewsPortal.Dal/NewsPortalDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsPortal.Dal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPortal.Dal
+{
+    public class NewsPortalDbContext: DbContext
+    {
+        public NewsPortalDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<News> News { get; set; }
+    }
+}
