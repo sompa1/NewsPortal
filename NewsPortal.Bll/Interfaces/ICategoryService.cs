6bdebb653c7f87b6c8e557e6fd61d016c2e8f90e
Panna Somlo <panna.somlo@sap.com>
2020-11-29 20:17:18 +0100

    Add category management pages

--- NewsPortal.Bll/Interfaces/ICategoryService.cs
+++ NewsPortal.Bll/Interfaces/ICategoryService.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using NewsPortal.Bll.Dtos;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace NewsPortal.Bll.Interfaces
@@ -10,7 +8,10 @@ namespace NewsPortal.Bll.Interfaces
     public interface ICategoryService
     {
         Task<List<CategoryDto>> GetCategories();
-
-        public IEnumerable<SelectListItem> GetAllCategory();
+        IEnumerable<SelectListItem> GetAllCategory();
+        Task<CategoryDto> GetCategoryById(int id);
+        Task UpdateCategoryById(CategoryDto dto);
+        Task CreateCategory(CategoryDto dto);
+        Task DeleteCategoryById(CategoryDto dto);
     }
 }
--- NewsPortal.Bll/Services/CategoryService.cs
+++ NewsPortal.Bll/Services/CategoryService.cs
@@ -4,10 +4,8 @@ using NewsPortal.Bll.Dtos;
 using NewsPortal.Bll.Interfaces;
 using NewsPortal.Dal;
 using NewsPortal.Model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace NewsPortal.Bll.Services
@@ -23,29 +21,65 @@ namespace NewsPortal.Bll.Services
 
         public Task<List<CategoryDto>> GetCategories()
         {
-            //var allCategories = DbContext.Categories.ToList();
+            return _dbContext
+                .Categories.Select(ent => new CategoryDto
+                {
+                    Id = ent.Id,
+                    Name = ent.Name
+                }).ToListAsync();
+        }
 
-            IQueryable<Category> query = _dbContext
-                .Categories.Include(c => c.News);
+        public Task<CategoryDto> GetCategoryById(int id)
+        {
+            return _dbContext
+                .Categories.Select(ent => new CategoryDto
+                {
+                    Id = ent.Id,
+                    Name = ent.Name
+                }).SingleAsync(ent => ent.Id == id);
+        }
 
-
-            return query.Select(c => new CategoryDto
+        public async Task DeleteCategoryById(CategoryDto dto)
+        {
+            var category = new Category()
             {
-                Id = c.Id,
-                Name = c.Name
-            }).ToListAsync();
+                Id = dto.Id,
+                Name = dto.Name
+            };
+            _dbContext.Categories.Remove(category);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateCategoryById(CategoryDto dto)
+        {
+            var category = new Category()
+            {
+                Id = dto.Id,
+                Name = dto.Name
+            };
+            _dbContext.Categories.Update(category);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task CreateCategory(CategoryDto dto)
+        {
+            var category = new Category()
+            {
+                Name = dto.Name
+            };
+            _dbContext.Categories.Add(category);
+            await _dbContext.SaveChangesAsync();
         }
 
         public IEnumerable<SelectListItem> GetAllCategory()
         {
-            IEnumerable<SelectListItem> list = _dbContext.Categories.Select(c => new SelectListItem
-            {
-                Selected = false,
-                Text = c.Name,
-                Value = (c.Id).ToString()
-            });
-
-            return list;
+            return _dbContext
+                .Categories.Select(c => new SelectListItem
+                {
+                    Selected = false,
+                    Text = c.Name,
+                    Value = (c.Id).ToString()
+                });
         }
     }
 }
